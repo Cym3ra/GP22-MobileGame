6a84f02ea430f7ed0048eae2457e2ff441153215
Cym3ra <112971484+Cym3ra@users.noreply.github.com>
2023-01-25 22:04:57 +0100

    fixes

---
+++ Plantastic/Assets/Scripts/PlantManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlantManager : MonoBehaviour
+{
+
+    [SerializeField] Sprite[] plantStages;
+    [SerializeField] SpriteRenderer plant;
+    [SerializeField] Button waterButton;
+
+    public int plantStage = 0;
+    public int wateringPressed = 0;
+    float timeBetweenStages = 2f;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //timer -= Time.deltaTime;
+    }
+
+    private void OnMouseDown()
+    {
+        //waterButton.gameObject.SetActive(true);
+    }
+
+    private void PlantInteraction(int watered)
+    {
+        wateringPressed += watered;
+
+        if (plantStages[0])
+        {
+            if (wateringPressed == 2)
+            {
+                plantStage++;
+                UpdatePlantStage();
+                Debug.Log("First");
+            }
+        }
+
+        if (plantStages[1])
+        {
+            if (wateringPressed == 5)
+            {
+                plantStage++;
+                UpdatePlantStage();
+                Debug.Log("Second");
+            }
+        }
+    }
+
+    public void Watering()
+    {
+        PlantInteraction(1);
+    }
+
+    private void UpdatePlantStage()
+    {
+        plant.sprite = plantStages[plantStage];
+    }
+}
