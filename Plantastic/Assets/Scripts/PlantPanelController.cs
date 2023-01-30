4924057030eb3862d808324a91c814420788bf69
Cym3ra <112971484+Cym3ra@users.noreply.github.com>
2023-01-30 12:05:46 +0100

    Plant panel changes made

---
+++ Plantastic/Assets/Scripts/PlantPanelController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlantPanelController : MonoBehaviour
+{
+
+    [SerializeField] Button waterButton;
+    [SerializeField] Button harvestButton;
+    [SerializeField] Image radialImage;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    public void WateringInput()
+    {
+
+    }
+
+    public void HarvestInput()
+    {
+
+    }
+}
