03e9ae950f0923f229a00d7d144c4fadb6dadbeb
Cym3ra <112971484+Cym3ra@users.noreply.github.com>
2023-02-09 15:55:36 +0100

    button interact testing

--- Plantastic/Assets/Scripts/BuyButtonController.cs
+++ Plantastic/Assets/Scripts/BuyButtonController.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class BuyButtonController : MonoBehaviour
 {
     public int buyPrice;
+    public bool hasBought = false;
 
     [SerializeField] Button buyButton;
 
@@ -23,7 +24,7 @@ public class BuyButtonController : MonoBehaviour
 
     public void CheckButtonInteractable()
     {
-        if (buyPrice <= CurrencyManager.instance.leavesAmount || buyPrice <= CurrencyManager.instance.flowerAmounts)
+        if (buyPrice <= CurrencyManager.instance.leavesAmount && !hasBought || buyPrice <= CurrencyManager.instance.flowerAmounts && !hasBought)
         {
             buyButton.GetComponent<Button>().interactable = true;
         }
@@ -32,4 +33,7 @@ public class BuyButtonController : MonoBehaviour
             buyButton.GetComponent<Button>().interactable = false;
         }
     }
+
+    // TO DO check if plant has been bought, and if it has, button is non-interactable, put it in check with CheckButtonInteractable
+    // add the script to the button component, and add a button listener if clicked: hasBought is true then(check if this works)
 }
